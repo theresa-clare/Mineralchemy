f0a8762ae765a6cca56189de7a57a0d5e72cdb20
Theresa Cay <theresacay@Theresa-Cays-MacBook-Pro.local>
2015-05-12 16:08:47 -0700

    Added login and signup route on server.py

--- server.py
+++ server.py
@@ -2,11 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
 	"""This is the homepage of Mineralchemy"""
-
 	return render_template("homepage.html")
+
+
+@app.route("/login")
+def login():
+	"""User login"""
+	return render_template("login_form.html")
+
+
+@app.route("/signup")
+def signup():
+	"""User sign up"""
+	return render_template("signup_form.html")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
