b2b140f04ffa00bc02a140bbd6962c5d99716d28
Theresa Cay <theresacay@Theresa-Cays-MacBook-Pro.local>
2015-05-12 22:49:48 -0700

    Added sign up functionality

--- server.py
+++ server.py
@@ -3,24 +3,47 @@ from model import User, connect_to_db, db
 
 
 app = Flask(__name__)
+app.secret_key = "mineral"
 
 
 @app.route("/")
 def index():
 	"""This is the homepage of Mineralchemy"""
+
 	return render_template("homepage.html")
 
 
 @app.route("/login")
 def login():
 	"""User login"""
+
 	return render_template("login_form.html")
 
 
-@app.route("/signup")
-def signup():
-	"""User sign up"""
+@app.route("/signup", methods=['GET'])
+def show_signup():
+	"""Render form for the user to sign up."""
+
 	return render_template("signup_form.html")
+
+
+@app.route("/signup", methods=['POST'])
+def signup():
+	"""Register user by adding user to User table and to session."""
+
+	email = request.form["email"]
+	password = request.form["password"]
+	firstname = request.form["firstname"]
+	lastname = request.form["lastname"]
+
+	new_user = User(email=email, password=password, firstname=firstname, lastname=lastname)
+
+	db.session.add(new_user)
+	db.session.commit()
+
+	flash("%s is now registered" % email)
+	return redirect("/")
+		
 
 @app.route("/search")
 def search():
