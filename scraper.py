72bc4568c7cb6cd3b40501647825a798ba2067e0
Theresa Cay <theresacay@Theresa-Cays-MacBook-Pro.local>
2015-05-25 22:44:57 -0700

    Filtered listing_id and scraped description from Minfind listing details page

--- scraper.py
+++ scraper.py
@@ -10,11 +10,19 @@ def scrape_minfind(keywords):
 	listings = soup.find_all("div", {"class":"minbox"})
 	for listing in listings:
 			listing_dict = {
+				"listing_id": int(filter(str.isdigit, listing.a['href'])),
 				"listing_origin": "minfind",
 				"url": "http://www.minfind.com/" + listing.a['href'],
 				"title": listing.find("div", {"class":"minboxmin"}).string.encode(encoding='UTF-8',errors='strict'),
 				"price": float(listing.find("div", {"class":"minboxprice"}).string[1:]),
 			}
+
+			details_html = urllib.urlopen(listing_dict["url"]).read()
+			details_soup = BeautifulSoup(details_html, "lxml")
+			main_content = details_soup.find("div", {"id":"maincontent"})
+
+			unicode_description = main_content.find("div", {"class":"fptext"}).contents[2]
+			listing_dict["description"] = unicode_description.encode(encoding='UTF-8',errors='strict').strip()
 
 			minfind_listings.append(listing_dict)
 
